f80cfbf9cb0b884fc6181bf56474ad44ba77ada9
Ihor Bodia <ihorbodia@gmail.com>
2020-08-06 23:10:34 +0100

    Add controls.

---
+++ src/WebScreenShooter/WebScreenShooter.Logic/Models/PlatformItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScreenShooter.Logic.Models
+{
+	public class PlatformItem
+	{
+		public string Name { get; set; }
+		public bool IsChecked { get; set; }
+	}
+}
--- src/WebScreenShooter/WebScreenShooter.UI/ViewModels/MainWindowViewModel.cs
+++ src/WebScreenShooter/WebScreenShooter.UI/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebScreenShooter.Logic.Models;
 
 namespace WebScreenShooter.UI.ViewModels
 {
-	public class MainWindowViewModel : ViewModelBase
-	{
-		public string Greeting => "Welcome to Avalonia!";
-	}
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string URL => "";
+        public string SitemapURL => "";
+        public string URLs => "";
+        public List<PlatformItem> Platfroms
+        {
+            get
+            {
+                return new List<PlatformItem>()
+        {
+          new PlatformItem() {Name = "DESKTOP 16:9" },
+          new PlatformItem() {Name = "DESKTOP 4:3" },
+          new PlatformItem() {Name = "MOBILE - HORIZONTAL" },
+          new PlatformItem() {Name = "MOBILE - VERTICAL" },
+          new PlatformItem() {Name = "IPHONE - HORIZONTAL" },
+          new PlatformItem() {Name = "IPHONE - VERTICAL" }
+        };
+            }
+        }
+    }
 }
