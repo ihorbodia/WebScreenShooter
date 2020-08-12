13079fc67f3beb757a9e501ca3222c3d9417ffe3
Ihor Bodia <ihorbodia@gmail.com>
2020-08-13 00:30:42 +0100

    Add logic.

---
+++ src/WebScreenShooter/WebScreenShooter.Logic/WebScreenShootWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScreenShooter.Logic
+{
+	public class WebScreenShootWorker
+	{
+		public async Task ParseXML(string xmlPath)
+		{
+			try
+			{
+				//Document doc = Jsoup.parse(new URL(xmlPath).openStream(), "UTF-8", "", Parser.xmlParser());
+
+				//for (Element e : doc.select("loc"))
+				//{
+				//	if (e.text().contains(".xml"))
+				//	{
+				//		parseXML(e.text());
+				//		mapHistory.add(e.text());
+				//	}
+				//	else
+				//	{
+				//		addURL(e.text());
+				//	}
+				//}
+			}
+			catch (IOException e)
+			{
+				// wrong url for sitemap
+				Console.WriteLine(e.StackTrace);
+			}
+		}
+
+		
+
+		
+	}
+}
--- src/WebScreenShooter/WebScreenShooter.UI/ViewModels/MainWindowViewModel.cs
+++ src/WebScreenShooter/WebScreenShooter.UI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using ReactiveUI;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reactive;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebScreenShooter.Logic;
 using WebScreenShooter.Logic.Models;
 
 namespace WebScreenShooter.UI.ViewModels
@@ -10,14 +14,21 @@ namespace WebScreenShooter.UI.ViewModels
         string url;
         string sitemapURL;
         string urls;
+        string timeout;
+
+        WebScreenShootWorker screenShooter;
 
         public MainWindowViewModel()
         {
-            Add = ReactiveCommand.Create(() => { });
-            GetURL = ReactiveCommand.Create(() => { });
-            Storage = ReactiveCommand.Create(() => { });
-            Reset = ReactiveCommand.Create(() => { });
-            Start = ReactiveCommand.Create(() => { });
+            Add = ReactiveCommand.CreateFromTask(AddHandler);
+            GetURLs = ReactiveCommand.CreateFromTask(GetURLsHandler);
+            Storage = ReactiveCommand.CreateFromTask(StorageHandler);
+            Reset = ReactiveCommand.CreateFromTask(ResetHandler);
+            Start = ReactiveCommand.CreateFromTask(StartHandler);
+            Exit = ReactiveCommand.CreateFromTask(ExitHandler);
+
+            urlsList = new List<string>();  //urls
+            urlHistory = new List<string>();
         }
 
         public string URL
@@ -36,11 +47,28 @@ namespace WebScreenShooter.UI.ViewModels
             set => this.RaiseAndSetIfChanged(ref urls, value);
         }
 
+        public string Timeout
+        {
+            get => timeout;
+            set 
+            {
+                if (int.TryParse(value, out _))
+                {
+                    this.RaiseAndSetIfChanged(ref timeout, value);
+                }
+                else
+                {
+                    Timeout = "0";
+                }
+            }
+        }
+
         public ReactiveCommand<Unit, Unit> Add { get; }
-        public ReactiveCommand<Unit, Unit> GetURL { get; }
+        public ReactiveCommand<Unit, Unit> GetURLs { get; }
         public ReactiveCommand<Unit, Unit> Storage { get; }
         public ReactiveCommand<Unit, Unit> Reset { get; }
         public ReactiveCommand<Unit, Unit> Start { get; }
+        public ReactiveCommand<Unit, Unit> Exit { get; }
 
         public ObservableCollection<PlatformItem> Platfroms
         {
@@ -59,5 +87,84 @@ namespace WebScreenShooter.UI.ViewModels
         }
 
 
+        private List<string> urlsList;
+        private List<string> urlHistory;
+
+        async Task AddHandler()
+        {
+            CheckArea();
+            //String urls[] = urlField.getText().split(",");
+            //for (String url : urls)
+            //{
+            //    addURL(url);
+            //}
+            //urlField.clear();
+        }
+
+        async Task GetURLsHandler()
+        {
+            //checkArea();
+            //addSitemap();
+        }
+
+        async Task StorageHandler()
+        {
+            //chooseStorage(directoryChooser);
+            //directoryChooser.setInitialDirectory(defaultDir);
+        }
+
+        async Task ResetHandler()
+        {
+            //reset();
+        }
+
+        async Task StartHandler()
+        {
+            CheckArea();
+            //timeout = waitField.getText().isEmpty() ? 9999 : Integer.valueOf(waitField.getText());
+            //startProgram();
+        }
+
+        async Task ExitHandler()
+        {
+            //endProgram();
+        }
+
+
+        void CheckArea()
+        {
+            string area = URLs;
+            URLs = string.Empty;
+            urlsList.Clear();
+            string[] urls = area.Split("\n");
+            foreach (var url in urls)
+            {
+                var value = Regex.Replace(url, "\\d*. ", "");
+                if (value.Length > 0)
+                {
+                    AddURL(url);
+                }
+            }
+        }
+
+        public async Task AddURL(string url)
+        {
+            if (url.Length == 0)
+            {
+                return;
+            }
+
+            if (url.Contains(".xml"))
+            {
+                SitemapURL = URL;
+                URL = string.Empty;
+            }
+            else
+            {
+                urlsList.Add(url);
+                URLs += urlsList.Count + ". " + url + "\n";
+                urlHistory.Add(url);
+            }
+        }
     }
 }
